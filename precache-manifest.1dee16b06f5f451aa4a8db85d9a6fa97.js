eb3e2c65ec379c987d009563882997635eefae0e
Anant Pratap Singh <anant.abes459@gmail.com>
2023-11-17 12:51:42 +0530

    Updates

---
+++ precache-manifest.1dee16b06f5f451aa4a8db85d9a6fa97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d98fd2a7c6eeddb013e259a3fa2811",
+    "url": "/TextWeb/index.html"
+  },
+  {
+    "revision": "1ba13a57957ae651322b",
+    "url": "/TextWeb/static/css/main.9914920d.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/TextWeb/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/TextWeb/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1ba13a57957ae651322b",
+    "url": "/TextWeb/static/js/main.e2318e38.chunk.js"
+  },
+  {
+    "revision": "c7d39de666860795314d",
+    "url": "/TextWeb/static/js/runtime~main.3f8e8948.js"
+  }
+]);
